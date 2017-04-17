97d2728e76f6f1a6bd4c5a61373211cbf785633b
cscovino <kikescovino@gmail.com>
2017-04-16 23:36:03 -0400

    update 16/04

--- MoveFabio/www/js/game.js
+++ MoveFabio/www/js/game.js
@@ -5,27 +5,44 @@ var app = {
 
     iniciaJuego: function(){
 
+        var alto = document.documentElement.clientHeight;
+        var ancho = document.documentElement.clientWidth;
         var button;
-        var background;
 
         function preload(){
-            game.physics.startSystem(Phaser.Physics.ARCADE);
+            //game.physics.startSystem(Phaser.Physics.ARCADE);
+            game.load.spritesheet('button','assets/butsprite.png',200,52);
         }
 
         function create(){
             game.stage.backgroundColor = '#182d3b';
+            button = game.add.button(10,10,'button',actionOnClick,this,1,0,1,0);
+
+            button.onInputOver.add(over, this);
+            button.onInputOut.add(out, this);
+            button.onInputUp.add(up, this);
         }
 
         function update(){
         }
 
+        function actionOnClick(){
+            game.stage.backgroundColor = '#0a4b3e';
+            button.destroy();
+        }
+
+        function over(){}
+        function out(){}
+        function up(){}
+
         var estados = {preload: preload, create: create, update: update};
-        var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser', estados);
+        var game = new Phaser.Game(ancho, alto, Phaser.AUTO, 'phaser', estados);
     },
+
 };
 
 if ('addEventListener' in document) {
-    document.addEventListener('deviceready', function(){
+    document.addEventListener('DOMContentLoaded', function(){
         app.inicio();
     }, false);
 }
