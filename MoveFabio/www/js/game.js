d8c7bffa9ab86b40d416959a2631fa379a6a8b98
cscovino <kikescovino@gmail.com>
2017-05-11 10:49:27 -0400

    with states 10/05

--- MoveFabio/www/js/game.js
+++ MoveFabio/www/js/game.js
@@ -1,139 +1,183 @@
 var app = {
 
-    inicio: function(){
-        app.iniciaJuego();
-    },
-
     iniciaJuego: function(){
 
         var alto = document.documentElement.clientHeight;
         var ancho1 = document.documentElement.clientWidth/2; 
         var ancho = document.documentElement.clientWidth;
-        var desks;
+        var desks, chairs;
         var button;
         var select;
+        var leftKey, rightKey, upKey, downKey, spaceKey;
 
-        function preload(){
-            //game.load.spritesheet('button','assets/butsprite.png',200,52);
-            //game.load.spritesheet('select','assets/selsprite.png',40,40);
-            game.load.spritesheet('fabio','assets/spritesheet3.png',55,96);
-            game.load.image('rect','assets/rect.png');
-            game.load.image('backgnd','assets/salon3.png');
+        var menu = {
+            preload: function(){
+                game.load.spritesheet('button','assets/butsprite.png',200,52);
+                
+            },
 
-            game.load.image('eylin','assets/eyin.png');
-            game.load.image('ifig','assets/ifi.png');
-            game.load.image('ronel','assets/ronel.png');
-            game.load.image('fab','assets/fabio.png');
-            game.load.image('mafe','assets/mafe.png');
-            game.load.image('smart','assets/smart.png');
-            game.load.image('sco','assets/yo.png');
-            game.load.image('ysa','assets/ysa.png');
-        }
+            create: function(){
+                game.stage.backgroundColor = '#182d3b';
 
-        function create(){
-            game.stage.backgroundColor = '#182d3b';
-            game.add.sprite(0,0,'backgnd');
+                button = game.add.button(10,10,'button',menu.actionOnClick,this,1,0,1,0);
+            },
 
-            //button = game.add.button(10,10,'button',actionOnClick,this,1,0,1,0);
+            update: function(){
+            },
 
-            desks = game.add.group();
-            desks.enableBody = true;
+            actionOnClick: function(){
+                game.state.start('room3');
+            },
+        };
 
-            var dfab = desks.create(0, game.world.height/6 - 2, 'fab');
-            dfab.body.immovable = true;
+        var room3State = {
+            preload: function(){
+                game.load.spritesheet('fabio','assets/spritesheet.png',55,96);
+                game.load.image('backgnd','assets/salon3.png');
+                game.load.image('chairfront', 'assets/silla.png');
+                game.load.image('chairback', 'assets/silla2.png');
 
-            var dsmart = desks.create(0, game.world.height/6 + 52, 'smart');
-            dsmart.body.immovable = true;
+                game.load.image('eylin','assets/eyin.png');
+                game.load.image('ifig','assets/ifi.png');
+                game.load.image('ronel','assets/ronel.png');
+                game.load.image('fab','assets/fabio.png');
+                game.load.image('mafe','assets/mafe.png');
+                game.load.image('smart','assets/smart.png');
+                game.load.image('sco','assets/yo.png');
+                game.load.image('ysa','assets/ysa.png');
+            },
 
-            var dey = desks.create(0, game.world.height - 315, 'eylin');
-            dey.body.immovable = true;
+            create: function(){
+                game.stage.backgroundColor = '#182d3b';
+                game.add.sprite(0,0,'backgnd');
 
-            var dsco = desks.create(0, game.world.height - 153, 'sco');
-            dsco.body.immovable = true;
+                leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+                rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+                upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+                downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+                spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
-            var dysa = desks.create(game.world.width - 120, game.world.height/6 - 2, 'ysa');
-            dysa.body.immovable = true;
+                chairs = game.add.group();
+                chairs.enableBody = true;
 
-            var dmafe = desks.create(game.world.width - 120, game.world.height/6 + 52, 'mafe');
-            dmafe.body.immovable = true;
+                desks = game.add.group();
+                desks.enableBody = true;
 
-            var difig = desks.create(game.world.width - 120, game.world.height - 315, 'ifig');
-            difig.body.immovable = true;
+                cfab = chairs.create(30, game.world.height/6 - 22, 'chairfront');
+                cfab.body.immovable = true;
+                var dfab = desks.create(0, game.world.height/6 - 2, 'fab');
+                dfab.body.immovable = true;
 
-            var dronel = desks.create(game.world.width - 120, game.world.height - 153, 'ronel');
-            dronel.body.immovable = true;
+                var csmart = chairs.create(30, game.world.height/6 + 91, 'chairback');
+                csmart.body.immovable = true;
+                var dsmart = desks.create(0, game.world.height/6 + 52, 'smart');
+                dsmart.body.immovable = true;
 
-            player = game.add.sprite(0,0,'fabio',4);
-            player.animations.add('down',[0,1,2,3],10,true);
-            player.animations.add('up',[4,5,6,7],10,true);
-            player.animations.add('right',[8,9,10,11],10,true);
-            player.animations.add('left',[12,13,14,15],10,true);
-            game.physics.arcade.enable(player);
-            player.body.collideWorldBounds = true;
+                var cey = chairs.create(30, game.world.height - 276, 'chairback');
+                cey.body.immovable = true;
+                var dey = desks.create(0, game.world.height - 315, 'eylin');
+                dey.body.immovable = true;
 
-            //select = game.add.sprite(200,200,'select');
-            //select.animations.add('spawn');
-            //select.animations.play('spawn',5,true);
-            //select.inputEnabled = true;
-            //select.events.onInputDown.add(path,this);
+                var csco = chairs.create(30, game.world.height - 115, 'chairback');
+                csco.body.immovable = true;
+                var dsco = desks.create(0, game.world.height - 153, 'sco');
+                dsco.body.immovable = true;
 
-            cursors = game.input.keyboard.createCursorKeys();
-        }
+                var cysa = chairs.create(game.world.width - 89, game.world.height/6 - 22, 'chairfront');
+                cysa.body.immovable = true;
+                var dysa = desks.create(game.world.width - 120, game.world.height/6 - 2, 'ysa');
+                dysa.body.immovable = true;
 
-        function update(){
-            player.body.velocity.x = 0;
-            player.body.velocity.y = 0;
+                var cmafe = chairs.create(game.world.width - 89, game.world.height/6 + 91, 'chairback');
+                cmafe.body.immovable = true;
+                var dmafe = desks.create(game.world.width - 120, game.world.height/6 + 52, 'mafe');
+                dmafe.body.immovable = true;
 
-            if (cursors.left.isDown) {
-                player.body.velocity.x = -190;
+                var cifig = chairs.create(game.world.width - 89, game.world.height - 276, 'chairback');
+                cifig.body.immovable = true;
+                var difig = desks.create(game.world.width - 120, game.world.height - 315, 'ifig');
+                difig.body.immovable = true;
 
-                player.animations.play('left');
-            }
-            else if (cursors.right.isDown) {
-                player.body.velocity.x = 190;
+                var cronel = chairs.create(game.world.width - 89, game.world.height - 115, 'chairback');
+                cronel.body.immovable = true;
+                var dronel = desks.create(game.world.width - 120, game.world.height - 153, 'ronel');
+                dronel.body.immovable = true;
 
-                player.animations.play('right');
-            }
-            else if (cursors.up.isDown) {
-                player.body.velocity.y = -190;
+                player = game.add.sprite(0,0,'fabio',4);
+                player.animations.add('down',[0,1,2,3],10,true);
+                player.animations.add('up',[4,5,6,7],10,true);
+                player.animations.add('right',[8,9,10,11],10,true);
+                player.animations.add('left',[12,13,14,15],10,true);
+                game.physics.arcade.enable(player);
+                player.body.collideWorldBounds = true;
 
-                player.animations.play('up');
-            }
-            else if (cursors.down.isDown) {
-                player.body.velocity.y = 190;
+                //select = game.add.sprite(200,200,'select');
+                //select.animations.add('spawn');
+                //select.animations.play('spawn',5,true);
+                //select.inputEnabled = true;
+                //select.events.onInputDown.add(path,this);
 
-                player.animations.play('down');
-            }
-            else{
-                player.animations.stop();
-                player.frame = 0;
-            }
+                //cursors = game.input.keyboard.createCursorKeys();
+            },
 
-            var hitDesks = game.physics.arcade.collide(player, desks);
-        }
+            update: function(){
+                player.body.velocity.x = 0;
+                player.body.velocity.y = 0;
 
-        function render(){
-            //game.debug.geom(rect, '#0fffff');
-        }
+                if (leftKey.isDown) {
+                    player.body.velocity.x = -190;
 
-        function actionOnClick(){
-            game.stage.backgroundColor = '#0a4b3e';
+                    player.animations.play('left');
+                }
+                else if (rightKey.isDown) {
+                    player.body.velocity.x = 190;
 
-            button.destroy();
+                    player.animations.play('right');
+                }
+                else if (upKey.isDown) {
+                    player.body.velocity.y = -190;
 
-            /*rect = game.add.sprite(0,0,'rect');
-            rect.inputEnabled = true;
-            rect.events.onInputDown.add(move, this);*/
-        }
+                    player.animations.play('up');
+                }
+                else if (downKey.isDown) {
+                    player.body.velocity.y = 190;
 
-        var estados = {preload: preload, create: create, update: update};
-        var game = new Phaser.Game(410, 630, Phaser.AUTO, 'phaser', estados);
+                    player.animations.play('down');
+                }
+                else if(spaceKey.isDown && checkOverlap()){
+                    console.log('action');
+                }
+                else{
+                    player.animations.stop();
+                    player.frame = 0;
+                }
+
+                var hitDesks = game.physics.arcade.collide(player, desks);
+            },
+
+            render: function(){
+                //game.debug.geom(rect, '#0fffff');
+            },
+
+            checkOverlap: function(){
+                var boundsA = player.getBounds();
+                var boundsB = cfab.getBounds();
+
+                return Phaser.Rectangle.intersects(boundsA,boundsB);
+            },
+        };
+            
+        var game = new Phaser.Game(410, 630, Phaser.AUTO, 'phaser');
+
+        game.state.add('room3', room3State);
+        game.state.add('menu', menu);
+        game.state.start('menu');
     },
 
 };
 
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function(){
-        app.inicio();
+        app.iniciaJuego();
     }, false);
 }
